015225d69d5adcac8b0ae3ceffe3a458c70c03a2
김새별 <@DESKTOP-BLS625F>
2022-10-17 21:28:43 +0900

    1017

---
+++ SemiProject/src/main/java/notice/controller/NoticeController.java
@@ -0,0 +1,11 @@
+package notice.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+
+@WebServlet("/notice/list")
+public class NoticeController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+}
