ac75a23890694a952d7d8c59e2f808b28b4bee21
김새별 <@DESKTOP-BLS625F>
2022-10-18 00:39:32 +0900

    test

---
+++ SemiProject/src/main/java/qna/controller/QnAController.java
@@ -0,0 +1,11 @@
+package qna.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+
+@WebServlet("/qna/list")
+public class QnAController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+}
