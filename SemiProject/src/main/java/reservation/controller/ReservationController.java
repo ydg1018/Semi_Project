b15fd4cdeaf4f493e35803bc35888edfbfdb93f8
user <user@192.168.219.101>
2022-10-19 21:49:50 +0900

    20221019 - 수정

--- SemiProject/src/main/java/reservation/controller/ReservationController.java
+++ SemiProject/src/main/java/reservation/controller/ReservationController.java
@@ -1,7 +1,6 @@
 package reservation.controller;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,7 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import login.dto.Hospital;
 import reservation.service.face.ReservationService;
 import reservation.service.impl.ReservationServiceImpl;
 
