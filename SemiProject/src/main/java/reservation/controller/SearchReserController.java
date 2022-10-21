f3a103e0edd1e7f3cf1d933ef222b70e2b239c2f
user <user@192.168.219.101>
2022-10-21 15:58:07 +0900

    20221021 - 수정

--- SemiProject/src/main/java/reservation/controller/SearchReserController.java
+++ SemiProject/src/main/java/reservation/controller/SearchReserController.java
@@ -22,6 +22,7 @@ public class SearchReserController extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("/search/reser [GET]");
 		
 		//전달파라미터에서 현재 페이징 객체 계산하기
 		Paging paging = searchReserService.getPaging(req);
