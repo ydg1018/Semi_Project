842a8a6a326274e5f8b60fda161c49824cbcc6c5
tnals <tnals@galaxybambi>
2022-10-22 20:18:37 +0900

    1022마이페이지

---
+++ SemiProject/src/main/java/mypage/controller/MypageOwnerDeleteController.java
@@ -0,0 +1,54 @@
+package mypage.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import login.dto.Owner;
+import mypage.service.face.MypageService;
+import mypage.service.impl.MypageServiceImpl;
+import util.Paging;
+
+/**
+ * 회원정보 삭제 컨트롤러
+ * 회원정보 삭제시 예약,게시글 등등 다른 테이블도 삭제해야하는지 미정
+ * 추후에 결정 후 만들어야함
+ * **/
+@WebServlet("/mypage/ownerDelete")
+public class MypageOwnerDeleteController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+private MypageService mypageService = new MypageServiceImpl();
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("/mypage/ownerDelete [GET]");
+		
+		//세션정보 객체
+		HttpSession session = req.getSession();
+		String owner_no = (String) session.getAttribute("owner_no");
+		//숫자형으로 변환 (임시로 1번 씀)
+		int ownerNo = 1; //Integer.parseInt(owner_no);;
+		
+		//서비스로 보낼 파라메터 데이터 셋팅
+		Owner param = new Owner();
+		param.setOwnerNo(ownerNo);
+		
+		//로그인한 Owner 정보 삭제
+		int data = mypageService.deleteOwner(param);
+		System.out.println("Owner 정보 삭제 data : " + data);
+
+		//조회결과를 MODEL값 전달
+		req.setAttribute("data", data);
+		
+		//View지정 및 응답
+		req.getRequestDispatcher("/WEB-INF/views/mypage/ownerUpdate.jsp").forward(req, resp);
+	}
+	
+}
---
+++ SemiProject/src/main/java/mypage/dao/impl/MypageDaoImpl.java
@@ -0,0 +1,217 @@
+package mypage.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import common.JDBCTemplate;
+import login.dto.Owner;
+import mypage.dao.face.MypageDao;
+import reservation.dto.Reservation;
+
+public class MypageDaoImpl implements MypageDao {
+	
+	private PreparedStatement ps; //SQL수행 객체
+	private ResultSet rs;	//SQL조회 결과 객체
+
+	@Override
+	public Owner getOwner(Connection conn, Owner param) {
+		System.out.println("MypageDao getOwner() - 시작");
+		
+		//SQL작성
+		String sql = "";
+		sql += "SELECT * FROM owner";
+		sql += " WHERE owner_no = ?";
+		
+		//결과 저정할 객체
+		Owner result = new Owner();
+		
+		try {
+			//SQL수행 객체
+			ps = conn.prepareStatement(sql); 
+			
+			//SQL 파라메터 셋팅
+			ps.setInt(1, param.getOwnerNo());
+			
+			//SQL수행 및 결과 집합 저장
+			rs = ps.executeQuery(); 
+			
+			//조회 결과 처리
+			while(rs.next()) {
+				result.setOwnerNo(rs.getInt("owner_no"));
+				result.setOwnerId(rs.getString("owner_id"));
+				result.setOwnerName(rs.getString("owner_name"));
+				result.setOwnerEmail(rs.getString("owner_email"));
+				result.setOwnerCall(rs.getInt("owner_call"));
+				result.setOwnerNick(rs.getString("owner_nick"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JDBCTemplate.close(rs);
+			JDBCTemplate.close(ps);
+		}
+		System.out.println("MypageDao getOwner() - 끝");
+		
+		return result; //최종 결과 반환
+	}
+	
+	@Override
+	public int updateOwner(Connection conn, Owner param) {
+		System.out.println("MypageDao updateOwner() - 시작");
+		
+		//SQL작성
+		String sql = "";
+		sql += "UPDATE owner SET ";
+		sql += "  owner_name = ?";
+		sql += " ,owner_email = ?";
+		sql += " ,owner_call = ?";
+		sql += " ,owner_nick = ?";
+		sql += " WHERE owner_no = ?";
+		
+		//결과 저정할 객체
+		int result = 0;
+		
+		try {
+			//SQL수행 객체
+			ps = conn.prepareStatement(sql); 
+			
+			//SQL 파라메터 셋팅
+			ps.setString(1, param.getOwnerName());
+			ps.setString(2, param.getOwnerEmail());
+			ps.setInt(3, param.getOwnerCall());
+			ps.setString(4, param.getOwnerNick());
+			ps.setInt(5, param.getOwnerNo());
+
+			//SQL수행 및 결과 집합 저장
+			result = ps.executeUpdate(); 
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JDBCTemplate.close(rs);
+			JDBCTemplate.close(ps);
+		}
+		System.out.println("MypageDao updateOwner() - 끝");
+		
+		return result; //최종 결과 반환
+	}
+	
+	@Override
+	public int deleteOwner(Connection conn, Owner param) {
+		System.out.println("MypageDao deleteOwner() - 시작");
+		
+		//SQL작성
+		String sql = "";
+		sql += "DELETE FROM owner";
+		sql += " WHERE owner_no = ?";
+		
+		//결과 저정할 객체
+		int result = 0;
+		
+		try {
+			//SQL수행 객체
+			ps = conn.prepareStatement(sql); 
+			
+			//SQL 파라메터 셋팅
+			ps.setInt(1, param.getOwnerNo());
+
+			//SQL수행 및 결과 집합 저장
+			result = ps.executeUpdate(); 
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JDBCTemplate.close(rs);
+			JDBCTemplate.close(ps);
+		}
+		System.out.println("MypageDao deleteOwner() - 끝");
+		
+		return result; //최종 결과 반환
+	}
+	
+	@Override
+	public Reservation getReservationOnwner(Connection conn, Reservation param) {
+		System.out.println("MypageDao getReservationOnwner() - 시작");
+		
+		//SQL작성
+		String sql = "";
+		sql += "SELECT * FROM reservation";
+		sql += " WHERE owner_no = ?";
+		
+		//결과 저정할 객체
+		Reservation result = new Reservation();
+		
+		try {
+			//SQL수행 객체
+			ps = conn.prepareStatement(sql); 
+			
+			//SQL 파라메터 셋팅
+			ps.setInt(1, param.getOwnerNo());
+			
+			//SQL수행 및 결과 집합 저장
+			rs = ps.executeQuery(); 
+			
+			//조회 결과 처리
+			while(rs.next()) {
+				result.setResNo(rs.getInt("res_no"));
+				result.setResDate(rs.getDate("res_date"));
+				result.setResDetail(rs.getString("res_detail"));
+				result.setOwnerNo(rs.getInt("owner_no"));
+				result.setPetNo(rs.getInt("pet_no"));
+				result.setHosNo(rs.getInt("hos_no"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JDBCTemplate.close(rs);
+			JDBCTemplate.close(ps);
+		}
+		System.out.println("MypageDao getReservationOnwner() - 끝");
+		
+		return result; //최종 결과 반환
+	}
+	
+	@Override
+	public Reservation getReservationHospital(Connection conn, Reservation param) {
+		System.out.println("MypageDao getReservationHospital() - 시작");
+		
+		//SQL작성
+		String sql = "";
+		sql += "SELECT * FROM reservation";
+		sql += " WHERE hosNo = ?";
+		
+		//결과 저정할 객체
+		Reservation result = new Reservation();
+		
+		try {
+			//SQL수행 객체
+			ps = conn.prepareStatement(sql); 
+			
+			//SQL 파라메터 셋팅
+			ps.setInt(1, param.getHosNo());
+			
+			//SQL수행 및 결과 집합 저장
+			rs = ps.executeQuery(); 
+			
+			//조회 결과 처리
+			while(rs.next()) {
+				result.setResNo(rs.getInt("res_no"));
+				result.setResDate(rs.getDate("res_date"));
+				result.setResDetail(rs.getString("res_detail"));
+				result.setOwnerNo(rs.getInt("owner_no"));
+				result.setPetNo(rs.getInt("pet_no"));
+				result.setHosNo(rs.getInt("hos_no"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JDBCTemplate.close(rs);
+			JDBCTemplate.close(ps);
+		}
+		System.out.println("MypageDao getReservationHospital() - 끝");
+		
+		return result; //최종 결과 반환
+	}
+}
